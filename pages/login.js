77fb40a02e6bdd5f6ebd930cab75dff17652c7aa
Jean Yang <bdlas321@gmail.com>
2018-04-03 11:22:54 +0800

    add: use custom server (express 4)
    ref: https://github.com/zeit/next.js/tree/master/examples/custom-server-express

---
+++ pages/login.js
@@ -0,0 +1,10 @@
+export default () => (
+  <div>
+    <h1>Login</h1>
+    <form>
+      <input id="login-account" type="text" placeholder="account" />
+      <input id="login-password" type="password" placeholder="password" />
+      <input id="login-submit" type="submit" value="login" />
+    </form>
+  </div>
+)
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 4000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+  .then(() => {
+    const server = express()
+
+    server.get('/login', (req, res) => app.render(req, res, '/login', req.query))
+
+    server.get('*', (req, res) => handle(req, res))
+
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
